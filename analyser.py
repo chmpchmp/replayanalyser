dd5214c3aa80aa27e2c5b03ae557e664eda6a0a6
chmpchmp <snoopnut@gmail.com>
2023-10-25 14:35:27 -0700

    updated analyser class

--- analyser.py
+++ analyser.py
@@ -7,32 +7,34 @@ class Analyser:
     def __init__(self, replay_path: str, songs_directory: str):
         self.beatmap = Beatmap(replay_path, songs_directory)
 
-        self.find_player_misses()
+        self.cursor_data = self.create_cursor_dictionary()
+        self.hit_object_data = self.create_hit_object_dictionary()
 
-    def find_player_misses(self):
-        ms_interval = 0
-        cursor_data = self.create_cursor_dictionary()
-        hit_object_data = self.create_hit_object_dictionary()
-        
-        while True:
-            if ms_interval in cursor_data.keys() and ms_interval in hit_object_data.keys():
-                print(ms_interval, cursor_data[ms_interval], hit_object_data[ms_interval])
-                
-            if ms_interval == 100000:
-                break
-            ms_interval += 1
+        self.find_player_misses()
 
     def create_cursor_dictionary(self) -> dict():
         cursor_dictionary = dict()
         ms_interval = 0
         for point in self.beatmap.replay_data['byte_array']:
-            ms_interval += int(point.split('|')[0])
-            cursor_dictionary[ms_interval] = point
+            split_list = point.split('|')
+            ms_interval += int(split_list[0])
+            cursor_dictionary[ms_interval] = split_list
 
         return cursor_dictionary
     
     def create_hit_object_dictionary(self) -> dict():
         return {int(hit_object[2]): hit_object for hit_object in self.beatmap.hit_object_data}
+
+    def find_player_misses(self):
+        ms_interval = 0
+        
+        while True:
+            if ms_interval in self.cursor_data.keys() and ms_interval in self.hit_object_data.keys():
+                print(ms_interval, self.cursor_data[ms_interval], self.hit_object_data[ms_interval])
+                
+            if ms_interval == 1000000:
+                break
+            ms_interval += 1
 
     def point_in_circle(self, center_x: int, center_y: int, radius: float, point_x: float, point_y: float) -> bool:
         return (point_x - center_x)**2 + (point_y - center_y)**2 < radius**2
