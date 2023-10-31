453276f8c3628f8fbcd17d71df65cfb99bb744be
chmpchmp <snoopnut@gmail.com>
2023-10-30 22:55:23 -0700

    created analyser_tests.py

---
+++ analyser_tests.py
@@ -0,0 +1,33 @@
+from replay import Replay
+from analyser import Analyser
+
+from dotenv import load_dotenv
+import os
+import unittest
+
+class AnalyserTestMethods(unittest.TestCase):
+
+    def test_replay_01(self):
+        replay_path = r"sample_replays\chmpchmp - Ni-Sokkususu - Blade Dance [Kneesocks] (2023-10-21) Osu.osr"
+
+        load_dotenv()
+        songs_directory = os.getenv('osu_songs_directory')
+
+        replay_data = Replay(replay_path).decode_replay()
+        beatmap = Analyser(replay_path, songs_directory)
+
+        self.assertEqual(replay_data['miss_count'], beatmap.break_count)
+
+    def test_replay_02(self):
+        replay_path = r"sample_replays\chmpchmp - Hana - Sakura no Uta [Euphoria] (2023-01-27) Osu.osr"
+
+        load_dotenv()
+        songs_directory = os.getenv('osu_songs_directory')
+
+        replay_data = Replay(replay_path).decode_replay()
+        beatmap = Analyser(replay_path, songs_directory)
+
+        self.assertEqual(replay_data['miss_count'], beatmap.break_count)
+
+if __name__ == '__main__':
+    unittest.main()
