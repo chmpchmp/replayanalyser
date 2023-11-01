5d0ede8df0c37ce434b1ed6821e9c486e0cf1296
chmpchmp <snoopnut@gmail.com>
2023-10-31 20:16:52 -0700

    added more tests

--- analyser_tests.py
+++ analyser_tests.py
@@ -6,7 +6,7 @@ import os
 import unittest
 
 class AnalyserTestMethods(unittest.TestCase):
-
+    # note: break_count - miss_count = sliderbreak_count
     def test_replay_01(self):
         replay_path = r"sample_replays\chmpchmp - Ni-Sokkususu - Blade Dance [Kneesocks] (2023-10-21) Osu.osr"
 
@@ -29,5 +29,16 @@ class AnalyserTestMethods(unittest.TestCase):
 
         self.assertEqual(replay_data['miss_count'], beatmap.break_count)
 
+    def test_replay_03(self):
+        replay_path = r"sample_replays\chmpchmp - ClariS - Hitorigoto -TV MIX- [Soliloquy] (2023-10-31) Osu.osr"
+
+        load_dotenv()
+        songs_directory = os.getenv('osu_songs_directory')
+
+        replay_data = Replay(replay_path).decode_replay()
+        beatmap = Analyser(replay_path, songs_directory)
+
+        self.assertEqual(replay_data['miss_count'], beatmap.break_count - 1)
+        
 if __name__ == '__main__':
     unittest.main()
