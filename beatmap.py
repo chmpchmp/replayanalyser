cd65add209a600478b7452c3b57107175ad80216
chmpchmp <snoopnut@gmail.com>
2023-11-02 00:27:30 -0700

    updated to support od and cs changes for hr and ez

--- beatmap.py
+++ beatmap.py
@@ -14,7 +14,7 @@ class Beatmap:
         self.difficulty_data = self.fetch_difficulty_data(self.directory, self.data['beatmap_id'])
         self.hit_object_data = self.fetch_hit_object_data(self.difficulty_data)
 
-        self.circle_radius = self.calculate_circle_radius(float(self.data['diff_size']))
+        self.circle_radius = self.calculate_circle_radius(float(self.data['diff_size']), int(self.replay.mods_used))
         self.hit_window = self.calculate_hit_window(float(self.data['diff_overall']), int(self.replay.mods_used))
 
         self.title = self.create_beatmap_title()
@@ -58,14 +58,36 @@ class Beatmap:
     def fetch_hit_object_data(self, difficulty_data: str) -> str:
         return [object.split(',')[:4] for object in difficulty_data.split('[HitObjects]')[1].split('\n')][1:-1]
     
-    def calculate_circle_radius(self, circle_size: float) -> float:
+    def calculate_circle_radius(self, circle_size: float, mods_used) -> float:
+        if mods_used & 16 == 16:    # circle size for hard rock
+            hr_circle_size = 1.3 * circle_size
+
+            if hr_circle_size < 10:
+                return (54.4 - 4.48 * hr_circle_size) 
+            else:
+                return (54.4 - 4.48 * 10)
+        
+        if mods_used & 256 == 256:    # circle size for easy
+            return (54.4 - 4.48 * circle_size * 0.5)
+
         return 54.4 - 4.48 * circle_size
     
     def calculate_hit_window(self, overall_difficulty: float, mods_used: int) -> float:
-        if mods_used & 64 == 64:    # hit window for double time mod (including nightcore)
+        if mods_used & 16 == 16:    # hit window for hard rock
+            hr_overall_difficulty = 1.4 * overall_difficulty
+
+            if hr_overall_difficulty < 10:
+                overall_difficulty = hr_overall_difficulty
+            else:
+                overall_difficulty = 10
+
+        if mods_used & 256 == 256:    # hit window for easy
+            overall_difficulty = 0.5 * overall_difficulty
+
+        if mods_used & 64 == 64:    # hit window for double time (including nightcore)
             return (200 - 10 * overall_difficulty) / 1.5
 
-        if mods_used & 256 == 256:    # hit window for half time mod
+        if mods_used & 256 == 256:    # hit window for half time
             return (200 - 10 * overall_difficulty) * 1.5
 
         return 200 - 10 * overall_difficulty
