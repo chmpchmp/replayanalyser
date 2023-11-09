d40b99ed541be3dbc0cd393afe02762bff6e62f9
chmpchmp <snoopnut@gmail.com>
2023-11-09 00:15:09 -0800

    created miss class

---
+++ miss.py
@@ -0,0 +1,10 @@
+class Miss:
+    def __init__(self):
+        self.missaim = False
+        self.no_input = False
+
+    def missaim(self):
+        self.missaim = True
+
+    def no_input(self):
+        self.no_input = True
