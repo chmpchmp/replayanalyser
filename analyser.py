682b63a5c07302410bb0130e80431105c3dfe84f
chmpchmp <snoopnut@gmail.com>
2023-11-13 18:24:52 -0800

    updated analyser.py

--- analyser.py
+++ analyser.py
@@ -1,7 +1,7 @@
 from beatmap import Beatmap
 from miss import Miss
 
-MILLISECOND_INTERVAL = 1000
+MILLISECOND_INTERVAL = 200
 
 class Analyser:
     def __init__(self, replay_path: str, songs_directory: str):
@@ -18,18 +18,16 @@ class Analyser:
 
         self.analyze_replay()
 
-        for miss in self.miss_data:
-            print(miss.hit_object_data)
-
         #print(self.miss_count, self.sliderbreak_count, self.break_count)
 
     def analyze_replay(self) -> None:
         # to do: account for hit window changes after breaks
         # to do: account for breaks from leaving sliderball and leaving buzzslider too early
 
-        cursor_data = self.beatmap.cursor_data
         active_cursor_points = self.fetch_active_cursor_points(self.beatmap.cursor_data)
         hit_object_data = self.beatmap.hit_object_data
+        cursor_data = self.beatmap.cursor_data
+        circle_radius = self.beatmap.circle_radius
 
         # set the window to the minimum timing of the first hit object
         previous_hit = [hit_object_data[0][2] - self.beatmap.hit_window, -1, -1, -1]
@@ -41,6 +39,7 @@ class Analyser:
             elif hit_object_data[i][3] & 8 == 8:
                 previous_hit = [hit_object_data[i+1][2] - self.beatmap.hit_window, -1, -1, -1]
             else:
+                # notelock misses occur when the input comes between the current object's minimum timing and previous object's maximum timing
                 possible_points = self.calculate_points_within_timing(hit_object_data[i][2], self.beatmap.hit_window, previous_hit[0], active_cursor_points)
                 possible_points = self.calculate_points_in_circle(hit_object_data[i][0], hit_object_data[i][1], self.beatmap.circle_radius, possible_points)
 
@@ -52,8 +51,9 @@ class Analyser:
 
                     miss_hit_object_data = [object for object in hit_object_data if hit_object_data[i][2] - MILLISECOND_INTERVAL <= object[2] <= hit_object_data[i][2] + MILLISECOND_INTERVAL]
                     miss_cursor_data = [point for point in cursor_data if hit_object_data[i][2] - MILLISECOND_INTERVAL <= point[0] <= hit_object_data[i][2] + MILLISECOND_INTERVAL]
+                    miss_cursor_input_data = [point for point in active_cursor_points if hit_object_data[i][2] - MILLISECOND_INTERVAL <= point[0] <= hit_object_data[i][2] + MILLISECOND_INTERVAL]
 
-                    self.miss_data.append(Miss(miss_hit_object_data, miss_cursor_data, hit_object_data[i][2]))
+                    self.miss_data.append(Miss(miss_hit_object_data, miss_cursor_data, hit_object_data[i][2], miss_cursor_input_data, circle_radius))
 
                     # set the window to the maximum timing to account for notelock
                     previous_hit = [hit_object_data[i][2] + self.beatmap.hit_window, -1, -1, -1]
@@ -89,8 +89,8 @@ class Analyser:
     def detect_key_two(self, first_timing: int, second_timing: int) -> bool:
         return first_timing & 2 == 0 and second_timing & 2 == 2
     
-    def calculate_points_within_timing(self, object_timing: float, hit_window: float, previous_hit_timing: float, cursor_points: list) -> list:
-        minimum_timing = previous_hit_timing
+    def calculate_points_within_timing(self, object_timing: float, hit_window: float, previous_max_hit_timing: float, cursor_points: list) -> list:
+        minimum_timing = previous_max_hit_timing
         maximum_timing = object_timing + hit_window
         return [point for point in cursor_points if minimum_timing < point[0] < maximum_timing]
     
