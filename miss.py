901f38f8959e46ae175ff6640f31ac39b0d5af77
chmpchmp <snoopnut@gmail.com>
2023-11-13 17:33:01 -0800

    updated miss class

--- miss.py
+++ miss.py
@@ -1,3 +1,5 @@
 class Miss:
-    def __init__(self, hit_object_data: list(list()), cursor_data: list(list()), reason: str):
-        pass
+    def __init__(self, hit_object_data: list(list()), cursor_data: list(list()), hit_object_timing: int):
+        self.hit_object_data = hit_object_data
+        self.cursor_data = cursor_data
+        self.hit_object_timing = hit_object_timing
