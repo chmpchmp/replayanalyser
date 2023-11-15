8ef3a23f05dd2ad2e527bc9969395b276b8fb57e
chmpchmp <snoopnut@gmail.com>
2023-11-15 11:40:24 -0800

    removed main statement

--- settings.py
+++ settings.py
@@ -9,6 +9,3 @@ class Settings:
         self.osu_directory = settings['DEFAULT']['osu_directory']
         self.songs_directory = f'{self.osu_directory}\Songs'
         self.replay_directory = f'{self.osu_directory}\Replays'
-
-if __name__ == '__main__':
-    Settings()
