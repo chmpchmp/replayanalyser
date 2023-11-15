d971b3a3109f1415547ee19e0aa8dfafc8381bb4
chmpchmp <snoopnut@gmail.com>
2023-11-15 14:22:56 -0800

    updated main

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from display import Display
 def run():
 
     settings = Settings()
-    replay_path = r"sample_replays\chmpchmp - kessoku band - Guitar to Kodoku to Aoi Hoshi [Akitoshi's Extreme] (2023-10-16) Osu.osr"
-    #replay_path = r"sample_replays\chmpchmp - ClariS - Hitorigoto -TV MIX- [Soliloquy] (2023-10-31) Osu.osr"
-    
-    songs_directory = settings.songs_directory
+    replay_path = input()
 
-    analyser = Analyser(replay_path, songs_directory)
+    analyser = Analyser(replay_path, Settings().songs_directory)
     display = Display(analyser.miss_data[0])
  
 if __name__ == '__main__':
