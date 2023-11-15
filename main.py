13ef9b49abf96959244cac118388fbeb5390c21f
chmpchmp <snoopnut@gmail.com>
2023-11-15 14:37:22 -0800

    updated main

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def run():
 
     settings = Settings()
     replay_path = input()
+    index = int(input())
 
     analyser = Analyser(replay_path, Settings().songs_directory)
-    display = Display(analyser.miss_data[0])
+    display = Display(analyser.miss_data[index])
  
 if __name__ == '__main__':
     run()
