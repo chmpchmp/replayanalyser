10e5ea31e8afb7d2d1f6e6c4ad6e111c0a1c3b10
chmpchmp <snoopnut@gmail.com>
2023-11-16 12:37:37 -0800

    updated to support local .env file as backup

--- settings.py
+++ settings.py
@@ -1,11 +1,23 @@
 import configparser
+from dotenv import load_dotenv
+import os
 
 class Settings:
     def __init__(self):
         settings = configparser.ConfigParser()
         settings.read('settings.ini')
 
+        load_dotenv()
+
         self.api_key = settings['DEFAULT']['api_key']
+
+        if len(self.api_key) == 0:
+            self.api_key = os.getenv('api_key')
+
         self.osu_directory = settings['DEFAULT']['osu_directory']
+
+        if len(self.osu_directory) == 0:
+            self.osu_directory = os.getenv('osu_directory')
+
         self.songs_directory = f'{self.osu_directory}\Songs'
         self.replay_directory = f'{self.osu_directory}\Replays'
