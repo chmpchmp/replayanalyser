99a8e35a2b50e880137d69b2986b476336a1749e
chmpchmp <snoopnut@gmail.com>
2023-12-04 00:45:08 -0800

    updated as dataclass

--- miss.py
+++ miss.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
 class Miss:
-    def __init__(self, hit_object_data: list(list()), cursor_data: list(list()), hit_object_timing: int, cursor_input_data: list(list()), circle_radius: float):
-        self.hit_object_data = hit_object_data
-        self.cursor_data = cursor_data
-        self.hit_object_timing = hit_object_timing
-        self.cursor_input_data = cursor_input_data
-        self.circle_radius = circle_radius
+    hit_object_data: list(list())
+    hit_object_timing: int
+    cursor_data: list(list())
+    cursor_input_data: list(list())
+    circle_radius: float
