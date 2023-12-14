09358748bc472de0a8c3cd613f0c9d15a2a95964
chmpchmp <snoopnut@gmail.com>
2023-12-13 22:31:29 -0800

    changed process class to data class

---
+++ data.py
@@ -0,0 +1,35 @@
+from settings import Settings
+from analyser import Analyser
+from canvas import Canvas
+
+import os
+
+FRAMES_DIRECTORY = 'frames'
+
+class Data:
+    def __init__(self, replay_path: str):
+        settings = Settings()
+        analyser = Analyser(replay_path, settings.songs_directory)
+
+        self.check_directory(FRAMES_DIRECTORY)
+        self.clear_directory(FRAMES_DIRECTORY)
+        self.generate_frames(FRAMES_DIRECTORY, analyser.miss_data)
+
+    @staticmethod
+    def check_directory(directory: str) -> None:
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+    @staticmethod
+    def clear_directory(directory: str) -> None:
+        for file in os.listdir(directory):
+            file_path = os.path.join(directory, file)
+            
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+    @staticmethod
+    def generate_frames(directory: str, miss_data: list) -> None:
+        for i, miss in enumerate(miss_data):
+            canvas = Canvas(miss)
+            canvas.export(f'{directory}\{i:06}')
