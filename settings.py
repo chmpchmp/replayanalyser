8d0287bb301ee67a3eb495eb281b7b82cca950e2
chmpchmp <snoopnut@gmail.com>
2023-12-19 18:29:53 -0800

    updated settings.py

--- settings.py
+++ settings.py
@@ -10,15 +10,8 @@ class Settings:
         settings.read(SETTINGS_PATH)
         load_dotenv()
 
-        self.api_key = settings['DEFAULT']['api_key']
-
-        if len(self.api_key) == 0:
-            self.api_key = os.getenv('api_key')
-
-        self.osu_directory = settings['DEFAULT']['osu_directory']
-
-        if len(self.osu_directory) == 0:
-            self.osu_directory = os.getenv('osu_directory')
+        self.api_key = settings['DEFAULT']['api_key'] or os.getenv('api_key')
+        self.osu_directory = settings['DEFAULT']['osu_directory'] or os.getenv('osu_directory')
 
         self.songs_directory = f'{self.osu_directory}\\Songs'
         self.replay_directory = f'{self.osu_directory}\\Replays'
